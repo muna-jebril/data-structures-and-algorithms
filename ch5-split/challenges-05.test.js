79ed62bc6cd40ab95234b806d63928dae04c37f2
muna-jebril <muna.jebril1995@hotmail.com>
2020-04-18 15:49:22 +0300

    finish it without schectch goal :

--- ch5-split/challenges-05.test.js
+++ ch5-split/challenges-05.test.js
@@ -86,7 +86,10 @@ const gruffaloCrumble = {
 
 const listFoods = (recipe) => {
   let result = [];
-  // Solution code here...
+  recipe['ingredients'].forEach(element => {
+    let z = element.slice(4,element.length).indexOf(' ');
+    result.push(element.slice(z+5,element.length));
+  });
   return result;
 };
 
@@ -100,7 +103,15 @@ You may also use other string or array methods.
 
 const splitFoods = (recipe) => {
   let result = [];
-  // Solution code here...
+  recipe['ingredients'].forEach(element => {
+    let a = element.split(' ');
+    let q ='';
+    for(let i =2;i<a.length;i++){
+      if(i===2) q += a[i] ;
+      else q += ' ' +a[i] ;
+    }
+    result.push(q);
+  });
   return result;
 };
 
@@ -116,7 +127,10 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 
 const stepActions = (recipe) => {
   let result = [];
-  // Solution code here...
+  recipe.steps.forEach(element => {
+    let z = element.split(' ')[0];
+    result.push(z);
+  });
   return result;
 };
 
@@ -134,7 +148,13 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const removeEvenValues = (arr) => {
-  // Solution code here...
+  for(let i =0;i<arr.length;i++){
+    if(arr[i]%2===0){
+      arr.splice(i,1);
+      i--;
+    }
+  }
+  console.log(arr);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -153,8 +173,10 @@ removeLastCharacters('Gregor', 9) returns ''
 ------------------------------------------------------------------------------------------------ */
 
 const removeLastCharacters = (str, numberOfCharacters) => {
-  // Solution code here...
-};
+  if(numberOfCharacters<0) return str;
+  if(numberOfCharacters>=str.length) return '';
+  let newStr = str.slice(0,str.length-numberOfCharacters);
+  return newStr;};
 
 
 /* ------------------------------------------------------------------------------------------------
